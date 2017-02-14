607c71270c223e6aba2fb4b4ce85b55065f4b5cb
colorlessenergy <brianmunozdev@gmail.com>
2017-02-13 19:56:42 -0800

    pulled words from api

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-var words = ["hello", "red", "orange", "black", "daily"];
 var chosenWord;
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var correctWord = [];
@@ -6,11 +5,11 @@ var displayWord = document.getElementById("scrambled-word");
 var input = document.getElementById("input");
 var send = document.getElementById("send");
 var guessedCorrect = false;
+
 setUp();
 
 function setUp() {
-  pickWord();
-  chooseCorrectLetters();
+  loadXMLDoc();
 }
 send.addEventListener("click", function () {
   if (input.value === chosenWord) {
@@ -30,20 +29,38 @@ input.addEventListener("keyup", function(event) {
   }
 })
 
-function pickWord () {
-  var wordsLength = words.length-1
-  var randomPos = Math.floor(Math.random() * wordsLength) + 0;
-  chosenWord = words[randomPos];
-}
 
-function chooseCorrectLetters () {
-  var splitWord = chosenWord.split("");
+function chooseCorrectLetters (word) {
+  console.log(chosenWord);
+  var splitWord = word.split("");
   for (var i = 0; i < letters.length; i++) {
    for (var j = 0; j < splitWord.length; j++) {
      if (letters[i] === splitWord[j]) {
        correctWord.push(letters[i]);
        displayWord.innerHTML = correctWord.join("");
+       console.log(correctWord);
      }
    }
  }
+}
+function loadXMLDoc() {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+           if (xmlhttp.status == 200) {
+             chosenWord = xmlhttp.responseText.toLowerCase();
+             chooseCorrectLetters(xmlhttp.responseText.toLowerCase());
+           }
+           else if (xmlhttp.status == 400) {
+              alert('There was an error 400');
+           }
+           else {
+               alert('something else other than 200 was returned');
+           }
+        }
+    };
+
+    xmlhttp.open("GET", "http://www.setgetgo.com/randomword/get.php", true);
+    xmlhttp.send();
 }
