af25c65749784e2fdd2b12bdc94ee00d014f7492
colorlessenergy <brianmunozdev@gmail.com>
2017-02-14 19:16:16 -0800

    added style

--- main.js
+++ main.js
@@ -4,18 +4,17 @@ var correctWord = [];
 var displayWord = document.getElementById("scrambled-word");
 var input = document.getElementById("input");
 var send = document.getElementById("send");
-var guessedCorrect = false;
-
 setUp();
 
 function setUp() {
   loadXMLDoc();
 }
+
 send.addEventListener("click", function () {
   if (input.value === chosenWord) {
     alert("you guessed correct ");
     input.value = "";
-    displayWord.innerHTML = "";
+    displayWord.innerHTML = "scrambled word: ";
     correctWord = [];
     setUp();
   } else {
@@ -29,6 +28,41 @@ input.addEventListener("keyup", function(event) {
   }
 })
 
+
+function chooseCorrectLetters (word) {
+  console.log(chosenWord);
+  var splitWord = word.split("");
+  for (var i = 0; i < letters.length; i++) {
+   for (var j = 0; j < splitWord.length; j++) {
+     if (letters[i] === splitWord[j]) {
+       correctWord.push(letters[i]);
+       displayWord.innerHTML = "scrambled word: " + correctWord.join("");
+       console.log(correctWord);
+     }
+   }
+ }
+}
+function loadXMLDoc() {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+           if (xmlhttp.status == 200) {
+             chosenWord = xmlhttp.responseText.toLowerCase();
+             chooseCorrectLetters(xmlhttp.responseText.toLowerCase());
+           }
+           else if (xmlhttp.status == 400) {
+              alert('There was an error 400');
+           }
+           else {
+               alert('something else other than 200 was returned');
+           }
+        }
+    };
+
+    xmlhttp.open("GET", "http://www.setgetgo.com/randomword/get.php", true);
+    xmlhttp.send();
+}
 
 function chooseCorrectLetters (word) {
   console.log(chosenWord);
