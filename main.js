a4e10d97a791727bc698a7e1b1945d552506400b
colorlessenergy <brianmunozdev@gmail.com>
2017-02-14 19:19:28 -0800

    fixed the code duplication

--- main.js
+++ main.js
@@ -63,38 +63,3 @@ function loadXMLDoc() {
     xmlhttp.open("GET", "http://www.setgetgo.com/randomword/get.php", true);
     xmlhttp.send();
 }
-
-function chooseCorrectLetters (word) {
-  console.log(chosenWord);
-  var splitWord = word.split("");
-  for (var i = 0; i < letters.length; i++) {
-   for (var j = 0; j < splitWord.length; j++) {
-     if (letters[i] === splitWord[j]) {
-       correctWord.push(letters[i]);
-       displayWord.innerHTML = correctWord.join("");
-       console.log(correctWord);
-     }
-   }
- }
-}
-function loadXMLDoc() {
-    var xmlhttp = new XMLHttpRequest();
-
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
-           if (xmlhttp.status == 200) {
-             chosenWord = xmlhttp.responseText.toLowerCase();
-             chooseCorrectLetters(xmlhttp.responseText.toLowerCase());
-           }
-           else if (xmlhttp.status == 400) {
-              alert('There was an error 400');
-           }
-           else {
-               alert('something else other than 200 was returned');
-           }
-        }
-    };
-
-    xmlhttp.open("GET", "http://www.setgetgo.com/randomword/get.php", true);
-    xmlhttp.send();
-}
